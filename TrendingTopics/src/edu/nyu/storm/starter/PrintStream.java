16ea8b9eb1ac7f67a717727f0ee9dc58ab98fbd1
Samit <samit.patel@nyu.edu>
2013-04-24 20:19:34 -0400

    Updated PrintStream

--- TrendingTopics/src/edu/nyu/storm/starter/PrintStream.java
+++ TrendingTopics/src/edu/nyu/storm/starter/PrintStream.java
@@ -4,11 +4,9 @@ package edu.nyu.storm.starter;
 // uncomment storm.starter.spout.TwitterSampleSpout, and uncomment this class
 
 
-import edu.nyu.storm.bolts.WordCounter;
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
 import backtype.storm.topology.TopologyBuilder;
-import backtype.storm.tuple.Fields;
 import backtype.storm.utils.Utils;
 import edu.nyu.storm.bolts.ExtractNoiseWords;
 import edu.nyu.storm.bolts.PrinterBolt;
@@ -25,14 +23,11 @@ public class PrintStream {
         	.shuffleGrouping("spout");
         builder.setBolt("noiseFilter", new ExtractNoiseWords())
     	.shuffleGrouping("tokenExtractor");
-//        builder.setBolt("word-normalizer", new WordNormalizer())
-//			.shuffleGrouping("tokenExtractor");
-//        builder.setBolt("word-counter", new WordCounter(),1)
-//		.fieldsGrouping("word-normalizer", new Fields("word"));
+//        builder.setBolt("wordCounter", new WordCounter(),1)
+//		.fieldsGrouping("noiseFilter", new Fields("filteredToken")); 
         builder.setBolt("print", new PrinterBolt())
-        	.shuffleGrouping("noiseFilter");
-    	builder.setBolt("word-counter", new WordCounter(),1)
-		.fieldsGrouping("noiseFilter", new Fields("word"));        
+    	.shuffleGrouping("noiseFilter");
+	
         
         Config conf = new Config();
         
