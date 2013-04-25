7f01831e934e0f9739c609bdbf5ca1703381a956
soo <ss6321@nyu.edu>
2013-04-24 20:16:35 -0400

    count

--- TrendingTopics/src/edu/nyu/storm/starter/PrintStream.java
+++ TrendingTopics/src/edu/nyu/storm/starter/PrintStream.java
@@ -4,9 +4,11 @@ package edu.nyu.storm.starter;
 // uncomment storm.starter.spout.TwitterSampleSpout, and uncomment this class
 
 
+import edu.nyu.storm.bolts.WordCounter;
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
 import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.tuple.Fields;
 import backtype.storm.utils.Utils;
 import edu.nyu.storm.bolts.ExtractNoiseWords;
 import edu.nyu.storm.bolts.PrinterBolt;
@@ -29,7 +31,8 @@ public class PrintStream {
 //		.fieldsGrouping("word-normalizer", new Fields("word"));
         builder.setBolt("print", new PrinterBolt())
         	.shuffleGrouping("noiseFilter");
-                
+    	builder.setBolt("word-counter", new WordCounter(),1)
+		.fieldsGrouping("noiseFilter", new Fields("word"));        
         
         Config conf = new Config();
         
